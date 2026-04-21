378159d7fde1223670e365e011f1b9b5c48ad294
Slip-Tech <lr93accs@gmail.com>
2026-04-21 21:48:04 +0100

    feat: basic F-Meter

---
+++ meters/f_meter/basic.py
@@ -0,0 +1,39 @@
+messages = ["Many F*cks", "Some F*cks", "No F*cks!!"]
+colours = ["lime", "yellow", "red"]
+current = 0
+
+def update():
+    global current
+
+    if badge.pressed(BUTTON_UP):
+        current = (current - 1) % len(messages)
+
+    if badge.pressed(BUTTON_DOWN):
+        current = (current + 1) % len(messages)
+  
+    if current == 1:
+        screen.pen = color.rgb(212, 206, 44)
+        screen.clear()
+    elif current == 2:
+        screen.pen = color.red
+        screen.clear()
+    else:
+        screen.pen = color.lime
+        screen.clear()
+        
+
+    # # draw a lighter rectangle as a banner
+    # screen.pen = color.rgb(60, 80, 120)
+    # screen.rectangle(rect(5, 40, screen.width - 10, 30))
+
+    # draw the text centred inside the banner
+    screen.pen = color.white
+    screen.font = rom_font.smart
+
+    text = messages[current]
+    width, _ = screen.measure_text(text)
+    x = (screen.width / 2) - (width / 2)
+
+    screen.text(text, x, 45)
+
+run(update)
